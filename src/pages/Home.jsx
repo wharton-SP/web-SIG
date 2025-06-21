3739d211d89bc4ca81f738dcff87368101dd7941
Wharton Aldrick (Sanctuary) <whartonaldrick@gmail.com>
2025-06-21 11:39:38 +0300

    Add : Display warning message about site development status on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,40 @@
 import { Wrench } from 'lucide-react'
+import { AlertTriangle } from 'lucide-react'
 import { Database } from 'lucide-react'
 import { Earth } from 'lucide-react'
 import React from 'react'
 
 const Home = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content text-center">
-                <div className="">
-                    <div className='flex flex-col items-center gap-4 mb-10'>
-                        <h1 className="text-5xl font-bold">Web S.I.G.</h1>
-                        <h3>(Système d'Information Géometrique)</h3>
+        <div>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content text-center">
+                    <div className="">
+                        <div className='flex flex-col items-center gap-4 mb-10'>
+                            <h1 className="text-5xl font-bold">Web S.I.G.</h1>
+                            <h3>(Système d'Information Géometrique)</h3>
+                        </div>
+                        <div className='flex gap-6'>
+                            <button className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl">
+                                <Earth />
+                                <span>Généralité</span>
+                            </button>
+                            <button className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl">
+                                <Wrench />
+                                <span>Outils</span>
+                            </button>
+                            <button className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl">
+                                <Database />
+                                <span>Données</span>
+                            </button>
+                        </div>
                     </div>
-                    <div className='flex gap-6'>
-                        <button className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl">
-                            <Earth />
-                            <span>Généralité</span>
-                        </button>
-                        <button className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl">
-                            <Wrench />
-                            <span>Outils</span>
-                        </button>
-                        <button className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl">
-                            <Database />
-                            <span>Données</span>
-                        </button>
-                    </div>
+                </div>
+            </div>
+            <div className="toast">
+                <div className="alert alert-warning shadow-lg">
+                    <AlertTriangle/>
+                    <span>Ce site est encore en développement, alors is se pourrait <br /> qu'il se comporte de façon inattendu</span>
                 </div>
             </div>
         </div>
