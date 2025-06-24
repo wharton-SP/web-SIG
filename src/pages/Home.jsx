d3a3c087c27a46bf4e8b9483d34b0dbbe8a36226
Wharton Aldrick (Sanctuary) <whartonaldrick@gmail.com>
2025-06-24 15:35:23 +0300

    Refactor : Simplify layout structure and improve alignment on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,18 @@ import { Wrench } from 'lucide-react'
 import { AlertTriangle } from 'lucide-react'
 import { Database } from 'lucide-react'
 import { Earth } from 'lucide-react'
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen">
-                <div className="hero-content text-center">
-                    <div className="">
-                        <div className='flex flex-col items-center gap-4 mb-10'>
+            <div className="bg-base-200 h-screen">
+                <div className="flex flex-col items-center justify-center h-full">
+                        <div className='flex flex-col items-center mb-10'>
                             <h1 className="text-5xl font-bold">Web S.I.G.</h1>
                             <h3>(Système d'Information Géometrique)</h3>
                         </div>
-                        <div className='flex gap-6'>
+                        <div className='md:flex grid gap-4'>
                             <Link to="/general" className="btn btn-neutral hover:btn-accent w-60 h-20 rounded-2xl flex gap-5">
                                 <Earth />
                                 <span>Généralité</span>
@@ -30,7 +28,6 @@ const Home = () => {
                             </Link>
                         </div>
                     </div>
-                </div>
             </div>
             <div className="toast">
                 <div className="alert alert-warning shadow-lg">
