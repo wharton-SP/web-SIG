aeca649f40a9ea6defe996d31af1014d40097f48
Wharton Aldrick (Sanctuary) <whartonaldrick@gmail.com>
2025-06-25 14:27:08 +0300

    Update: Change header text from 'Généralité' to 'Outils' in Tools page for better clarity

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -7,7 +7,7 @@ const Tools = () => {
             <Navigation/>
             <Content>
                 <div className='flex flex-col gap-4 '>
-                    <h1 className='text-2xl font-bold'>Bienvenue dans la section Généralité</h1>
+                    <h1 className='text-2xl font-bold'>Bienvenue dans la section Outils</h1>
                     <p>Cette section est en cours de développement. Vous pouvez y trouver des informations sur le site, les outils et les données.</p>
                     <p>Restez à l'écoute pour plus de contenu !</p>
                 </div>
